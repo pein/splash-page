caf727ffaf9b134ceedeeecf144a4971379482fc
Ilyes Houha <mi.houha@gmail.com>
2020-12-24 09:54:59 +0100

    Change background function

---
+++ script.js
@@ -0,0 +1,28 @@
+const { body } = document;
+
+function changeBackground(number) {
+  let previousBackground = "";
+  body.className = "";
+  if (body.className) {
+    previousBackground = body.className;
+  }
+  switch (number) {
+    case "1":
+      return previousBackground === "background-1"
+        ? false
+        : body.classList.add("background-1");
+      break;
+    case "2":
+      return previousBackground === "background-2"
+        ? false
+        : body.classList.add("background-2");
+      break;
+    case "3":
+      return previousBackground === "background-3"
+        ? false
+        : body.classList.add("background-3");
+      break;
+    default:
+      break;
+  }
+}
